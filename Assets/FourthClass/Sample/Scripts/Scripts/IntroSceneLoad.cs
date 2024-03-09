94e58c098dddc59558c1fc98787c312664d60e49
sbssu <dlfjswodlf@naver.com>
2024-03-09 17:58:52 +0900

    (2024.03.09) 로딩 씬 구현과 대화 시스템 구현
    Loading Scene 생성 및 씬 간 동기 호출
    UI 제작 및 아이콘, 대화 출력 구현

--- Assets/FourthClass/Sample/Scripts/Scripts/IntroSceneLoad.cs
+++ Assets/FourthClass/Sample/Scripts/Scripts/IntroSceneLoad.cs
@@ -7,6 +7,7 @@ public class IntroSceneLoad : MonoBehaviour
 {
     public void LoadGameScene()
     {
-        SceneManager.LoadScene("GameScene");
+        //SceneManager.LoadScene("GameScene");
+        Loading.LoadScene("GameScene");
     }
 }
